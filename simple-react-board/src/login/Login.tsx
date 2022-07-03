074a0805fd2aed7f26987a85856e1eff890a92ef
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-07-03 22:17:16 +0900

    Create Login.tsx

---
+++ simple-react-board/src/login/Login.tsx
@@ -0,0 +1,19 @@
+import {Component} from 'react';
+
+class Login extends Component {
+  constructor(props: any){
+    super(props);
+  }
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
